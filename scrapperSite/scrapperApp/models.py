96b2db35521b4bc8e42f850cb9a65f40ea1de922
yashgarg1 <51264866+yashgarg1@users.noreply.github.com>
2023-06-02 12:33:50 +0530

    added update and get data

--- scrapperSite/scrapperApp/models.py
+++ scrapperSite/scrapperApp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create a model for the cryptocurrency data.
 class Cryptocurrency(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FloatField()
-    one_h_percent = models.FloatField()
-    twentyFour_h_percent = models.FloatField()
-    seven_d_percent = models.FloatField()
-    market_cap = models.FloatField()
-    volume_24h = models.FloatField()
-    circulating_supply = models.FloatField()
+    price = models.CharField(max_length=255)
+    one_h_percent = models.CharField(max_length=255)
+    twentyFour_h_percent = models.CharField(max_length=255)
+    seven_d_percent = models.CharField(max_length=255)
+    market_cap = models.CharField(max_length=255)
+    volume_24h = models.CharField(max_length=255)
+    circulating_supply = models.CharField(max_length=255)
--- scrapperSite/scrapperApp/views.py
+++ scrapperSite/scrapperApp/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from scrapperApp.scraper import scrape_coinmarketcap
-from scrapperApp.updateData import *
+from scrapperApp.models import *
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def index(request):
     data = scrape_coinmarketcap()
-    request.data = data
-    update_data(request)
+    # request.data = data
+    # update_data(request)
     return HttpResponse(data)
+
+
+# Create a view that accepts the HTTP POST request and updates the data in the database.
+@csrf_exempt 
+def update_data(request):
+    # Get the data from the request.
+    data = request.body.decode('utf-8')
+    
+    # Parse the JSON data
+    data = json.loads(data)
+    print(data)
+    data_list = data["data"]
+    
+    # Create a cryptocurrency object for each piece of data.
+    for row in data_list:
+        cryptocurrency = Cryptocurrency(
+            name=row["name"],
+            price=row["price"],
+            one_h_percent=row["1h%"],
+            twentyFour_h_percent=row["24h%"],
+            seven_d_percent=row["7d%"],
+            market_cap=row["market_cap"],
+            volume_24h=row["volume(24h)"],
+            circulating_supply=row["circulating_supply"],
+        )
+        cryptocurrency.save()
+
+
+    return HttpResponse("Data updated successfully.")
+
+
+def get_latest_data(request):
+    # Get the latest cryptocurrency data from the database.
+    cryptocurrency_data = Cryptocurrency.objects.all()
+
+    data = serializers.serialize('json', cryptocurrency_data)
+    
+    return JsonResponse(data, safe=False)
